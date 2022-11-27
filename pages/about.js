9d3e7667e86ac70c4ca0e3679196d042394ec1ee
Gaurav Burande <100035026+GauravBurande@users.noreply.github.com>
2022-11-27 22:22:38 +0600

    ignore

--- pages/about.js
+++ pages/about.js
@@ -11,7 +11,6 @@ const About = () => {
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi inventore, id repudiandae possimus ratione asperiores, voluptatem rem ducimus officiis autem quia perferendis deleniti similique placeat sit voluptatibus quam enim ea nihil! Dolores necessitatibus deserunt dolore mollitia, numquam repellendus aliquam explicabo?</p>
         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos architecto recusandae consectetur ducimus, magnam cumque dignissimos vel deleniti repellat modi earum itaque nobis doloremque pariatur nesciunt laborum maiores unde, ad neque omnis ea, fugit doloribus sint optio! Officiis ducimus et facilis necessitatibus, molestias obcaecati expedita natus eius sint voluptates architecto repudiandae cum quod cumque autem in at magni eveniet id! Minima pariatur architecto illo illum rem totam?</p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam natus, dolores repudiandae amet praesentium eos aliquid aut cum eum est quaerat dolorem dicta voluptatum voluptas officia quod maiores corporis iste?</p>
-      </div>
 
       <h2>Services</h2>
       <p>Lorem ipsum dolor sit amut consectetur adipisicing elit. Id excepturi doloribus ipsam beatae aut quibusdam dicta optio consequatur, consectetur cupiditate minima ex error, placeat voluptatum eos sunt dolore ducimus exercitationem natus! Debitis repudiandae dolorem quaerat suscipit accusamus dicta iste veniam ex magnam praesentium facilis consectetur mollitia quidem neque hic aspernatur eius quisquam doloremque, cum pariatur numquam eligendi inventore! Repellat, quidem!</p>
@@ -27,6 +26,8 @@ const About = () => {
       <h2>Contact us</h2>
       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil doloribus eveniet quos amet, impedit exercitationem tempora velit. Iure maiores aut eveniet, harum dolor quae, deleniti repellendus nemo molestias beatae dolores.</p>
     </div>
+    </div>
+
   )
 }
 
