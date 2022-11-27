6864689419adfb3210d3a1a9625f8c524765a09b
Gaurav Burande <100035026+GauravBurande@users.noreply.github.com>
2022-11-27 12:39:06 +0600

    basic style and dummy about page

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import styles from '../styles/about.module.css'
 
 const About = () => {
   return (
-    <div>
-      This is about component
+    <div className={styles.container}>
+      <div className={styles.main}>
+        <h1>About BlogBuster</h1>
+
+        <h2>Intoduction</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi inventore, id repudiandae possimus ratione asperiores, voluptatem rem ducimus officiis autem quia perferendis deleniti similique placeat sit voluptatibus quam enim ea nihil! Dolores necessitatibus deserunt dolore mollitia, numquam repellendus aliquam explicabo?</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos architecto recusandae consectetur ducimus, magnam cumque dignissimos vel deleniti repellat modi earum itaque nobis doloremque pariatur nesciunt laborum maiores unde, ad neque omnis ea, fugit doloribus sint optio! Officiis ducimus et facilis necessitatibus, molestias obcaecati expedita natus eius sint voluptates architecto repudiandae cum quod cumque autem in at magni eveniet id! Minima pariatur architecto illo illum rem totam?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam natus, dolores repudiandae amet praesentium eos aliquid aut cum eum est quaerat dolorem dicta voluptatum voluptas officia quod maiores corporis iste?</p>
+      </div>
+
+      <h2>Services</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id excepturi doloribus ipsam beatae aut quibusdam dicta optio consequatur, consectetur cupiditate minima ex error, placeat voluptatum eos sunt dolore ducimus exercitationem natus! Debitis repudiandae dolorem quaerat suscipit accusamus dicta iste veniam ex magnam praesentium facilis consectetur mollitia quidem neque hic aspernatur eius quisquam doloremque, cum pariatur numquam eligendi inventore! Repellat, quidem!</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, possimus. Voluptates magni molestiae dolorum consectetur officiis veniam enim facilis corporis magnam a eaque dolorem, dicta quam ullam harum hic doloremque tempora illo mollitia labore. Vitae praesentium temporibus fuga iusto tempore quod reprehenderit odit accusamus dolorum!</p>
+      <p>We offer these Services</p>
+      <ul>
+        <li>service 1</li>
+        <li>service 2</li>
+        <li>service 3</li>
+        <li>service 4</li>
+      </ul>
+
+      <h2>Contact us</h2>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil doloribus eveniet quos amet, impedit exercitationem tempora velit. Iure maiores aut eveniet, harum dolor quae, deleniti repellendus nemo molestias beatae dolores.</p>
     </div>
   )
 }
